19589399fb4628824eb313d02fbb194da6dc34c6
김현정 <ajdrns8694@naver.com>
2021-09-02 18:28:42 +0900

    0

---
+++ refer-effect/assets/js/custom.js
@@ -0,0 +1,21 @@
+    //Code view
+    document.addEventListener('DOMContentLoaded', (event) => {
+        document.querySelectorAll('pre code').forEach((el) => {
+            hljs.highlightElement(el);
+        })
+    })
+
+    //Code tab menu
+
+
+    //Modal
+    document.querySelector(".info button").addEventListener("click", function(){
+        document.querySelector("#modal").classList.add("show");
+        document.querySelector("#modal").classList.remove("hide");
+    });
+    
+    document.querySelector(".modal-cont button").addEventListener("click", function(){
+        document.querySelector("#modal").classList.add("hide");
+    });
+    
+
