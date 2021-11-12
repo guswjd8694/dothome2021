9da25da2ce9a5de5767ee1e56538d26a722821bc
DALLAE <82562349+guswjd8694@users.noreply.github.com>
2021-11-12 20:20:28 +0900

    pop

--- portfolio/assets/js/app.js
+++ portfolio/assets/js/app.js
@@ -6,7 +6,7 @@ class App {
     constructor(){
         this.canvas = document.createElement('canvas');
         this.ctx = this.canvas.getContext('2d');
-        document.body.appendChild(this.canvas);
+        document.getElementById("waves").append(this.canvas);
 
         //Wave 생성
         this.waveGroup = new WaveGroup();
